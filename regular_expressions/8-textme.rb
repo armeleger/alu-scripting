f3185b3cd2788ec9e6d13797d429bbc0bf10c793
armeleger <a.kayisire@alustudent.com>
2025-07-09 12:46:11 +0200

    a simple commit.

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+<<<<<<< HEAD
 input = ARGV[0]
 match = input.match(/\[from:(.*?)\].*?\[to:(.*?)\].*?\[flags:(.*?)\]/)
 
@@ -12,3 +13,16 @@ else
   puts "No match found"
 end
 
+=======
+input = ARGV[0]
+match = input.match(/\[from:(.*?)\].*?\[to:(.*?)\].*?\[flags:(.*?)\]/)
+
+if match
+  sender = match[1]
+  receiver = match[2]
+  flags = match[3]
+  puts "#{sender},#{receiver},#{flags}"
+else
+  puts "No match found"
+end
+>>>>>>> 4bdfa1b6a304f78c4fa2dd56eeca0ab09ce803f0
